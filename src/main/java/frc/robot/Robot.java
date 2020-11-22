7c5e8d171c2b190885c2bf5b035077132d71cb81
KrishKittur <73321517+KrishKittur@users.noreply.github.com>
2020-11-21 19:48:22 -0800

    Added differential drive class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,36 +14,35 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
+This arcade drive is powered using only the left analog stick 
+Movements on the y-axis of the analog stick will move the robot forwards/backwards
+Movements on the x-axis of the analog stick will turn the robot 
  */
 public class Robot extends TimedRobot {
 
-  /* Initialize the motors */
+  /* Initialize the motors and differential drive */
   WPI_TalonSRX motorRight = new WPI_TalonSRX(1);
   WPI_TalonSRX motorLeft = new WPI_TalonSRX(4);
+  DifferentialDrive motorDrive = new DifferentialDrive(motorLeft, motorRight);
 
   /* Initalize the Xbox controller */
   XboxController controller = new XboxController(1);
 
+  /* In the robot init function, set the motors equal to 0 just to "reset" the robot */
   @Override
   public void teleopInit() {
     motorRight.set(0);
     motorLeft.set(0);
   }
 
+  /* In the teleopPeriodic loop get controller inputs and input them into the arcade drive function */
   @Override
   public void teleopPeriodic() {
-  
-    motorRight.set(controller.getY(Hand.kRight));
-    motorLeft.set(controller.getY(Hand.kLeft));
-
+    motorDrive.arcadeDrive(controller.getY(Hand.kLeft), controller.getX(Hand.kLeft));
   }
 }
